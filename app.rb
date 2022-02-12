ba2f8de8a32cdc25693418dcd425d075ad358b80
Daisuke Ueda　上田 大輔 <daisuke.u524@gmail.com>
2022-02-12 12:09:58 +0900

    Improve: フォームに入力された値を表示させる

--- app.rb
+++ app.rb
@@ -3,3 +3,11 @@ require "sinatra"
 get '/' do
   erb :index
 end
+
+post '/create' do
+  point = params[:point]
+  comment = params[:comment]
+  "ポイント :#{point}　コメント :#{comment}"
+end
+
+# please access this URL: http://localhost:4567
